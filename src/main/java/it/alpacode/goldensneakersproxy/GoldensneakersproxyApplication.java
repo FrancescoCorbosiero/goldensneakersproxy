5bdd60a505b6c0c1c9f77aac06c2a5f8dda96b20
FrancescoCorbosiero <francesco.corbosiero00@gmail.com>
2025-12-29 05:19:07 +0100

    Spring Initializer

---
+++ src/main/java/it/alpacode/goldensneakersproxy/GoldensneakersproxyApplication.java
@@ -0,0 +1,13 @@
+package it.alpacode.goldensneakersproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldensneakersproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldensneakersproxyApplication.class, args);
+	}
+
+}
